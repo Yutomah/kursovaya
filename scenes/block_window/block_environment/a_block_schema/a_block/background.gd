d6d53c7a6592a80d4f2b8ce812f5e3d086ea6ca2
Yutomah <tevict@yandex.ru>
2024-06-10 15:05:16 +0500

    Вроде разобрался с изначальной настройкой новых блоков

---
+++ scenes/block_window/block_environment/a_block_schema/a_block/background.gd
@@ -0,0 +1,20 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var img = Image.new()
+	img.load(texture_normal.resource_path)
+	var bitmap = BitMap.new()
+	bitmap.create_from_image_alpha(img)
+	texture_click_mask = bitmap
+
+
+
+
+
+func _on_pressed():
+	print("-----")
+	print_debug(size)
+	print_debug($"..".size)
+	print_debug($"../..".size)
